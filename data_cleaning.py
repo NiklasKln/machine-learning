1db29571bd19c1725469fc159e899e6dce157ae8
niklaskln <info.niklasklein@gmail.com>
2021-10-01 13:55:30 +0200

    better formatting

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,11 +1,7 @@
 
-
 # function that deletes all columns containing a NULL value
 
 def deleteColumns(X_train, X_valid):
     cols_with_missing = [col for col in X_train.columns
      if X_train[col].isnull().any()] 
-return X_train.drop(cols_with_missing, axis=1), X_valid.drop(cols_with_missing,axis=1)
-
-
-
+    return X_train.drop(cols_with_missing, axis=1), X_valid.drop(cols_with_missing ,axis=1)
